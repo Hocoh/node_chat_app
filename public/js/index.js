0e324be06c73b1adf1209b9a26ec2622b8ff474b
J.Doe <sunning@protonmail.com>
2018-07-22 00:37:24 +0200

    pipeline

--- public/js/index.js
+++ public/js/index.js
@@ -60,13 +60,26 @@ socket.on("connect", function () {
             return alert("Geolocation failed on browser")
         }
 
-        navigator.geolocation.getCurrentPosition(
-            function(position){ 
-            console.log(position);
-            }, 
-            function (){ 
-            alert("Unable to fetch location")
-            })
+        var options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+          };
+          
+          function success(pos) {
+            var crd = pos.coords;
+          
+            console.log('Your current position is:');
+            console.log(`Latitude : ${crd.latitude}`);
+            console.log(`Longitude: ${crd.longitude}`);
+            console.log(`More or less ${crd.accuracy} meters.`);
+          }
+          
+          function error(err) {
+            console.warn(`ERROR(${err.code}): ${err.message}`);
+          }
+          
+          navigator.geolocation.getCurrentPosition(success, error, options);
     });
 });
 
